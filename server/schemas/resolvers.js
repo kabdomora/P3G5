b3440db6a52ff51a11aef6f277d537b54525795e
kabdomora <114969831+kabdomora@users.noreply.github.com>
2023-03-27 19:19:11 -0700

    starter code and file structure

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,14 @@
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require('apollo-server-express');
+
+const resolvers = {
+    Query: {
+        
+    },
+    Mutation: {
+        
+    },
+};
+
+module.exports = resolvers;

135b1bdb1bee593370d720ceb5039153921f2055
VulnusR <caleb.lockwood626@gmail.com>
2023-03-29 00:33:43 -0700

    modal updated

--- client/src/components/Donate.js
+++ client/src/components/Donate.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import DonationModal from './DonationModal';
 
 
 
 function Donate() {
+
+    
     return (
         <div className='main-parent'>
             
             <div className='main-article-parent'>
                 <div className='donate-parent'>
-                    <div className='donate-btn'>
+                    <input type={'button'} className='donate-btn' />
 
-                    </div>
+                    
                 </div>
             </div>
         </div>
--- client/src/components/DonationModal.js
+++ client/src/components/DonationModal.js
@@ -15,4 +15,41 @@ function DonationModal(props) {
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log(`Donating ${donationAmount} to ${selectedPet}`);
-  };
+};
+
+return (
+    <div className="modal">
+      <div className="modal-content">
+        <span className="close" onClick={props.onClose}>
+          &times;
+        </span>
+        <form onSubmit={handleSubmit}>
+          <h2>Make a Donation</h2>
+          <div>
+            <label htmlFor="pets">Select a Pet:</label>
+            <select id="pets" value={selectedPet} onChange={handlePetSelect}>
+              <option value="">Select a Pet</option>
+              {props.pets.map((pet) => (
+                <option key={pet.id} value={pet.name}>
+                  {pet.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div>
+            <label htmlFor="amount">Select an Amount:</label>
+            <select id="amount" value={donationAmount} onChange={handleAmountSelect}>
+              <option value="0">Select an Amount</option>
+              <option value="5">$5</option>
+              <option value="10">$10</option>
+              <option value="20">$20</option>
+            </select>
+          </div>
+          <button type="submit">Donate</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default DonationModal;

11a2ff6fbb1d7272977dd8831cd99a0cfa0718c5
Mab <mabchhan@gmail.com>
2023-03-30 21:31:27 -0700

    added supply seed data

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,12 +1,17 @@
 const db = require("../config/connection");
-const { User, Pet } = require("../models");
+const { User, Pet, Supply } = require("../models");
 
 const petSeeds = require("./petSeeds.json");
 const userSeeds = require("./userSeeds.json");
+const supplySeeds = require("./supplyData.json");
+
 db.once("open", async () => {
   try {
     await Pet.deleteMany({});
     await Pet.insertMany(petSeeds);
+
+    await Supply.deleteMany({});
+    await Supply.insertMany(supplySeeds);
 
     await User.deleteMany({});
     await User.insertMany(userSeeds);
